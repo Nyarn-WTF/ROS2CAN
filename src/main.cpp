5d4015f9954095301ac6432cbe5712bfd0a2c2ec
hogepiyo <s16054@tokyo.kosen-ac.jp>
2019-10-05 01:53:26 +0900

    [fix]Segmentation falt

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,48 @@
 #include <Arduino.h>
-#include <ros2can.hpp>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <ros2arduino.h>
+#include <CAN.h>
 
-ROS2CAN *ros2can;
+#define SSID       "SSID"
+#define SSID_PW    "PW"
+#define AGENT_IP   "IPADDRESS"
+#define AGENT_PORT 2019
+
+class ROS2CAN : public ros2::Node{
+private:
+public:
+  ROS2CAN():Node("RobotNode"){
+    Serial.end();
+    Serial.begin(115200);
+    while(!Serial);
+    ros2::init(&Serial);
+  }
+
+  ROS2CAN(WiFiUDP *udp):Node("RobotNode"){
+    WiFi.begin(SSID, SSID_PW);
+    while(WiFi.status() != WL_CONNECTED);
+    ros2::init(udp, AGENT_IP, AGENT_PORT);
+  }
+
+  void begin(){
+
+  }
+
+  void stop(){
+
+  }
+};
+
+ROS2CAN* node;
 
 void setup() {
-  ros2can = new ROS2CAN(ROSWIFI);
-  //ros2can->begin();
+  enableCore1WDT();
+  node = new ROS2CAN();
 }
 
 void loop() {
-  //ros2::spin(ros2can);
+  disableCore1WDT();
+  enableCore1WDT();
+  ros2::spin(node);
 }
